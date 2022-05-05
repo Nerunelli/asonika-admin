5c6a1f04e5b6c96e4ee443ce9254ac4d7297cb75
Ferunelli <ferunelli@meetsidekick.com>
2022-05-06 02:18:14 +0300

    effects refactor typings

--- src/data/manufacturers/effects.ts
+++ src/data/manufacturers/effects.ts
@@ -2,30 +2,30 @@ import { createEffect } from 'effector';
 import { api } from '../../api/useApi';
 import { ICreateManufacturerPayload, IManufacturer, IUpdateManufacturerPayload } from './types';
 
-export const loadManufacturerFx = createEffect<() => Promise<IManufacturer[]>>(async () => {
+export const loadManufacturerFx = createEffect<void, IManufacturer[]>(async () => {
   const res = await api.get('/manufacturer/');
   return res.data.data;
 });
 
-export const createManufacturerFx = createEffect<
-  (_payload: ICreateManufacturerPayload) => Promise<IManufacturer>
->(async ({ name, description }) => {
-  const res = await api.post('/manufacturer/', {
-    name,
-    description,
-  });
+export const createManufacturerFx = createEffect<ICreateManufacturerPayload, IManufacturer>(
+  async ({ name, description }) => {
+    const res = await api.post('/manufacturer/', {
+      name,
+      description,
+    });
 
-  return res.data.data;
-});
+    return res.data.data;
+  },
+);
 
-export const updateManufacturerFx = createEffect<
-  (_payload: IUpdateManufacturerPayload) => Promise<IManufacturer>
->(async ({ uuid, name, description }) => {
-  await api.put<{ data: IManufacturer }>(`/manufacturer/${uuid}/`, { description, name });
-  return { uuid, name, description };
-});
+export const updateManufacturerFx = createEffect<IUpdateManufacturerPayload, IManufacturer>(
+  async ({ uuid, name, description }) => {
+    await api.put<{ data: IManufacturer }>(`/manufacturer/${uuid}/`, { description, name });
+    return { uuid, name, description };
+  },
+);
 
-export const deleteManufacturerFx = createEffect<(_uuid: string) => Promise<string>>(async uuid => {
+export const deleteManufacturerFx = createEffect<string, string>(async uuid => {
   await api.delete<{ data: IManufacturer }>(`/manufacturer/${uuid}/`);
   return uuid;
 });
